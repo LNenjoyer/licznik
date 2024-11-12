6735924e7f8f8911f607fe3bf8dc183e804ddec2
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-12 13:47:00 +0100

    nie

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function Example() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                +
+            </button>
+            <button onClick={() => setCount(count - 1)}>
+                -
+            </button>
+        </div>
+    );
+}
+export default Example;
