0a6aa94e9c289a09e93ecedf44a2facd64324851
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-13 09:00:26 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import React, { useState } from 'react';
 
 function Example() {
     const [count, setCount] = useState(0);
-
     return (
         <div>
             <p>You clicked {count} times</p>
             <button onClick={() => setCount(count + 1)}>
                 +
             </button>
-            <button onClick={() => setCount(count - 1)}>
+            <button onClick={() => setCount(count<1 ? count : count - 1)}>
                 -
             </button>
         </div>
