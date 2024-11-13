ba3e2cbf0c95bd436fe4f68400cbb33caa3e393d
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-13 10:13:00 +0100

    aaaaaaaadokumentacjaaaAAAAAAAaaaaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import React, { useState } from 'react';
 
 function Example() {
     const [count, setCount] = useState(0);
+    // dosłownie zmienna, dlaczego utrudniać życie czymś takim, PO CO?!!
     return (
         <div>
             <p>You clicked {count} times</p>
+            {/*wyświetlanie ilości kliknięć*/}
             <button onClick={() => setCount(count + 1)}>
                 +
             </button>
+            {/*przycisk by zwiększyć ilość kliknięć*/}
             <button onClick={() => setCount(count<1 ? count : count - 1)}>
                 -
             </button>
+            {/*przycisk by zmiejszyć ilość kliknięć, z uwzględzieniem faktu że nie może być mniej niż 0*/}
         </div>
     );
 }
